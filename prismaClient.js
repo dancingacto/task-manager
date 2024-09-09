3c61ef7c56952e555615223a748f91faca63a809
dancingactor <s990346@gmail.com>
2024-09-09 17:56:10 +0800

    og the DATABASE_URL in your backend code to ensure that it is being read correctly

--- prismaClient.js
+++ prismaClient.js
@@ -1,4 +1,4 @@
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
-
+console.log('DATABASE_URL:', process.env.DATABASE_URL);  // Log this to ensure it's being used correctly
 module.exports = prisma;
