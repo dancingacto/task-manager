857d814e7b2437daa938846a5911e9f533f574f2
dancingactor <s990346@gmail.com>
2024-09-17 21:23:25 +0800

    Verify JWT is working correctly, remove the log message to prevent JWT leaks

--- routes/users.js
+++ routes/users.js
@@ -20,9 +20,6 @@ router.post('/login', async (req, res) => {
         return res.status(401).json({ error: 'Invalid email or password' });
     }
 
-    console.log(`JWT Secret Key: ${process.env.JWT_SECRET}`);
-    
-
     const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET);
     res.json({ token });
 });
